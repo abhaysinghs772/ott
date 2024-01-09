27da95825f792043540e069c0aa7c86c88ced127
abhaysinghs772 <abhaysinghs772@gmail.com>
2024-01-09 12:07:23 +0530

    initial commit for my own ott platform

---
+++ client/snowpack.config.js
@@ -0,0 +1,12 @@
+// snowpack.config.js
+module.exports = {
+    mount: {
+        src: '/',
+    },
+    plugins: [
+        '@snowpack/plugin-typescript',
+    ],
+    buildOptions: {
+        htmlFragments: true,
+    },
+};
